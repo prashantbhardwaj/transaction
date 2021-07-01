3067538bb03952dc921e2611ce34abda30a7a0d7
Prashant Bhardwaj <amu.prashant@gmail.com>
2021-07-01 23:38:58 +0100

    first commit

---
+++ src/test/java/com/prashant/transactionsystem/TransactionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.transactionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
